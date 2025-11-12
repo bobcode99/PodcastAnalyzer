1c9ee5bc71a5c69e3d47610558af348778007600
yb <lojobs38@gmail.com>
2025-11-12 12:59:59 +0800

    Initial Commit

---
+++ PodcastAnalyzerTests/PodcastAnalyzerTests.swift
@@ -0,0 +1,16 @@
+//
+//  PodcastAnalyzerTests.swift
+//  PodcastAnalyzerTests
+//
+//  Created by Bob on 2025/11/12.
+//
+
+import Testing
+
+struct PodcastAnalyzerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
